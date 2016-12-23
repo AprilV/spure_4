38b09d4c07e943ed362f401d5decb0dc79cbd974
wsgdrfz <wsgdrfzv@gmail.com>
2016-12-23 15:19:52 +0800

    add def about controller sneaker

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,2 +1,50 @@
 class SneakersController < ApplicationController
+  def index
+  	@sneaker = Sneaker.paginate(page: params[:page])
+  end
+
+  def show
+  	@sneaker = Sneaker.find(params[:id])
+  	redirect_to root_url 
+  end
+
+  def new 
+  	@sneaker = Sneaker.new
+  end
+
+  def create
+  	@sneaker = Sneaker.new(sneaker_params)
+  	if @sneaker.save
+  		flash[:info] = "sneaker save!"
+  		redirect_to root_url
+  	else
+  		render 'new'
+  	end
+  end
+
+  def edit
+  	@sneaker = Sneaker.find(params[:id])
+  end
+
+  def update
+  	@sneaker = Sneaker.find(params[:id])
+  	if @sneaker.update_attributes(sneaker_params)
+  		flash[:sucess] = "sneaker update"
+  		redirect_to @user
+  	else
+  		render 'edit'
+  	end
+  end
+
+  def destroy
+  	Sneaker.find(params[:id]).destroy
+  	flash[:success] = "sneaker destroy"
+  	redirect_to sneakers_url
+  end
+
+  private 
+    def sneaker_params
+    	params.require(:sneaker).permit(:image,:max_size,:min_size,:history_high,
+    									:history_low,:sneaker_type_id,:sneaker_type_name)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -69,6 +69,8 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
+
   
   private
 
