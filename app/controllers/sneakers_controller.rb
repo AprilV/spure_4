35df0b5b572b95099254720cc3730f8508f6312b
wsgdrfz <wsgdrfzv@gmail.com>
2016-12-23 15:30:21 +0800

    add quanxian

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,4 +1,7 @@
 class SneakersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:index]
+  before_action :admin_user,     only: [:destroy,:update,:new,:update]
   def index
   	@sneaker = Sneaker.paginate(page: params[:page])
   end
@@ -46,5 +49,23 @@ class SneakersController < ApplicationController
     def sneaker_params
     	params.require(:sneaker).permit(:image,:max_size,:min_size,:history_high,
     									:history_low,:sneaker_type_id,:sneaker_type_name)
+    end
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
